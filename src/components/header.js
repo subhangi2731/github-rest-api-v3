b001d0e4fb9c4d342be9c9883858003814838338
subhangi2731 <subhangichoudhary6@gmail.comgit config --global user.name subhangi2731git config --global user.email subhangichoudhary6@gmail.com>
2020-12-14 07:59:13 +0530

    added github star and fork buttons

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,20 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import {useHistory } from 'react-router-dom';
 
 const Header = () => {
-	const history=useHistory();
-	const handleClick=()=>{
-		history.push('/github-rest-api-v3/compare');
-	}
-	return (
-		<header >
-			<Navbar color='primary' dark>
-				<NavbarBrand href='./'>GitHub API Playground</NavbarBrand>
-				<button className="btn btn-success" onClick={handleClick}>Compare Users</button>
-			</Navbar>
-		</header>
-	);
+const history=useHistory();
+const handleClick=()=>{
+history.push('/github-rest-api-v3/compare');
+}
+return (
+<header >
+<Navbar color='primary' dark>
+<NavbarBrand href='./'>GitHub API Playground</NavbarBrand>
+<iframe src="https://ghbtns.com/github-btn.html?user=adityabisoi&repo=github-rest-api-v3&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+<iframe src="https://ghbtns.com/github-btn.html?user=adityabisoi&repo=github-rest-api-v3&type=fork&count=true&size=large" frameborder="0" scrolling="0" width="158px" height="30px"></iframe>
+<button className="btn btn-success" onClick={handleClick}>Compare Users</button>
+</Navbar>
+</header>
+);
 };
 
 export default Header;
